6d9522f3a3d84a27f85c32991d12e154122ed468
palolik <azizulalamprottoy@gmail.com>
2024-08-07 17:39:33 +0600

    sliders fixed

--- app/src/main/java/com/kotlin5/edumanager/data/CourseModel.kt
+++ app/src/main/java/com/kotlin5/edumanager/data/CourseModel.kt
@@ -2,13 +2,13 @@ package com.kotlin5.edumanager.data
 
 data class CourseModel (
 val title : String?,
-val image : String?
-,val price : String?
-,val description : String?
-,val userEmail : String?
-,val userName : String?
-,val totalenrolment : String?
-    , val status : String?
+val image : String?,
+val price : String?,
+val description : String?,
+val userEmail : String?,
+val userName : String?,
+val totalenrolment : String?,
+val status : String?
 
 
 )
