c7009ed0679a574b78c3e2ec7f01b4ed6dd8bae0
palolik <azizulalamprottoy@gmail.com>
2024-08-12 12:10:11 +0600

    auth pages added

---
+++ app/src/main/java/com/kotlin5/edumanager/presentation/auth/SignInActivity.kt
@@ -0,0 +1,15 @@
+package com.kotlin5.edumanager.presentation.auth
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.kotlin5.edumanager.R
+import com.kotlin5.edumanager.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/kotlin5/edumanager/presentation/auth/SignUpActivity.kt
@@ -0,0 +1,5 @@
+package com.kotlin5.edumanager.presentation.auth
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity:AppCompatActivity(){}
